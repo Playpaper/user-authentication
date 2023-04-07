436cb850342c80cf6722c0c733f58c5d0b537f16
Playpaper <heartcolorful@gmail.com>
2023-04-07 21:39:47 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 
 if(process.env.NODE_ENV !== 'production') {
@@ -18,8 +19,14 @@ db.once('open', () => console.log('mongodb connected !'))
 const app = express()
 const PORT = 3000
 
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send(`It's an user authentication web app`)
+  res.render('index')
 })
 
 app.listen(PORT, (req, res) =>{
